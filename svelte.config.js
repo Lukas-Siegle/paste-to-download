de798298f6cdf7822260dc5ce2bc1b081e3afb3c
Lukas-Siegle <sieglelukas1@gmail.com>
2024-11-03 10:56:23 +0100

    u

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,24 @@
 import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-	  adapter: adapter({
-		pages: 'build',
-		assets: 'build',
-		fallback: 'index.html',
-		precompress: false,
-		strict: true
-	  }),
-	  paths: {
-		base: process.env.NODE_ENV === 'production' ? '/paste-to-download' : '',
-	  }
-	}
-  };
+  preprocess: preprocess({
+    typescript: true,
+    postcss: true
+  }),
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html',
+      precompress: false,
+      strict: true
+    }),
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '/paste-to-download' : '',
+    }
+  }
+};
 
 export default config;
